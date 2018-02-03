48b7bb65188501c22e5734de1e93a806eb84ed37
Michael A Craig <michael.a.craig@warbyparker.com>
2018-02-03 13:01:30 -0500

    Refactor to allow more flexible data pulling

--- ml_boilerplate/externaldata/census_pull.py
+++ ml_boilerplate/externaldata/census_pull.py
@@ -3,6 +3,7 @@
 import os
 import pandas as pd
 from census import Census
+from itertools import chain
 from functools import reduce
 from dotenv import load_dotenv
 from dotenv import find_dotenv
@@ -34,7 +35,7 @@ def columns_for_table(table, line_nums):
     return ['{}_{}'.format(table, l) for l in line_ends]
 
 
-def pull_census_columns(cols, census_api):
+def pull_census_columns(cols, census_api=None):
     """ Pull a given list of census columns.
 
         :param cols: a list of columns to pull
@@ -43,50 +44,71 @@ def pull_census_columns(cols, census_api):
 
         :return: a DataFrame where the geography is the index and
             the columns are the columns. """
-    print('Pulling columns {}'.format(cols))
-    df = pd.DataFrame(census_api.acs5.zipcode(['NAME'] + cols, '*'))
-    df['NAME'] = df['NAME'].map(lambda x: x[-5:])
+    if census_api is None:
+        census_api = Census(census_key())
 
-    return (df.rename(columns={'NAME': 'zipcode'})
-              .set_index('zipcode')[cols]
-              .apply(pd.to_numeric, errors='coerce'))
+    def pull_chunk(col_chunk):
+        # Census API only allows 50 columns per call
+        print('Pulling columns {}'.format(col_chunk))
+        df = pd.DataFrame(census_api.acs5.zipcode(['NAME'] + col_chunk, '*'))
+        df['NAME'] = df['NAME'].map(lambda x: x[-5:])
+        return (df.rename(columns={'NAME': 'zipcode'})
+                  .set_index('zipcode')[col_chunk]
+                  .apply(pd.to_numeric, errors='coerce'))
+
+    chunk_fn = lambda c, n: (c[i:i+n] for i in range(0, len(c), n))
+    return reduce(lambda x, y: x.join(y),
+                  (pull_chunk(chunk) for chunk in chunk_fn(cols, 49)))
 
 
-def income_variables(census=None):
-    """ Pull various income variables for all zipcodes.
-
-        :param census: a Census API object
+def income_variables():
+    """ Return the list of income variables to pull from.
 
         :return: A DataFrame with the income variables as the columns
             and the geography as the index. """
-    if census is None:
-        census_api = Census(census_key())
-
-    cols = columns_for_table('B19001', range(2, 18))
-    return pull_census_columns(cols, census_api)
+    return columns_for_table('B19001', range(2, 18))
 
 
-def educational_attainment(census=None):
-    """ Pull educational variables for all zipcodes.
-
-        :param census: a Census API object
+def educational_attainment():
+    """ Return the list of educational attainment variables to pull from.
 
         :return: A DataFrame with the educational variables as the
             columns and the geography as the index. """
-    if census is None:
-        census_api = Census(census_key())
-
-    cols = columns_for_table('B15003', range(16, 26))
-    return pull_census_columns(cols, census_api)
+    return columns_for_table('B15003', range(16, 26))
 
 
-def census_pipeline(pipeline):
+def age_variables():
+    """ Return the list of age variables to pull from.
+
+        :return: A list of column names to pull. """
+    return columns_for_table('B01001', range(2, 50))
+
+
+def population_variables():
+    """ Return the list of population variables to pull from.
+
+        :return: A list of columns names to pull. """
+    return columns_for_table('B00001', range(1, 2))
+
+
+def census_pipeline(col_pipeline):
     """ Given a list of functions, calls them all and joins them to get
         a census dataset.
 
-        :param pipeline: a list of functions to combine
+        :param col_pipeline: a list of functions that return the list of
+            columns needed to be pulled.
 
         :return: a DataFrame with all the variables as columns and zipcode
             as the index. """
-    return reduce(lambda x, y: pd.merge(x, y, left_index=True, right_index=True),
-                  (func() for func in pipeline))
+    cols = set(chain.from_iterable(f() for f in col_pipeline))
+    return pull_census_columns(list(cols))
+
+
+if __name__ == '__main__':
+    # Example use: build a dataset by pulling income, education, age,
+    # and population by zipcode and save it out.
+    df = census_pipeline([income_variables,
+                          educational_attainment,
+                          age_variables,
+                          population_variables])
+    df.to_csv('census_dataset.csv')
